d6f07c30625b1ff18c1fec2e28c06300111a7fd8
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-05 12:12:39 +0100

    finish task

---
+++ js/styles.js
@@ -0,0 +1,16 @@
+function drawTree(x) {
+        for ( i=0; i<x; i++ ) {
+            var star = ' ';
+            var space =' ';
+            for (j=0; j<=i * 2; j++)
+                {
+                star += '*';
+                }
+            for (k=i +1 ; k<x; k++) 
+                {
+                space += ' ';
+                }
+            console.log(space,star);
+        }
+    }
+    console.log(drawTree(10))
